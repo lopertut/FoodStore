75a8c3e0e34233664a3d11edd9da9df3c02b7584
lopertut <artur.matvejev12345@gmail.com>
2024-09-13 13:12:47 +0300

    products app added

--- FoodStore/settings.py
+++ FoodStore/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "authentication.apps.AuthenticationConfig"
+    "authentication.apps.AuthenticationConfig",
+    "products.apps.ProductsConfig"
 ]
 
 MIDDLEWARE = [
